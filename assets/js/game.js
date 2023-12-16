54e0e2bdff6ce58268b390c78c8ce6494a8b5c46
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 17:36:54 +1030

    Add questions module and import it in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,5 @@
+import { questions } from './modules/questions.js';
+
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
@@ -13,41 +15,6 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let timeRemaining = 60;
-
-let questions = [
-    {
-        question: 'What brackets are used for an array?',
-        choice1: '[]',
-        choice2: '()',
-        choice3: '{}',
-        choice4: '<>',
-        answer: 1,
-    },
-    {
-        question: 'What does "var" stand for?',
-        choice1: 'various',
-        choice2: 'variable',
-        choice3: 'varied',
-        choice4: 'varnish',
-        answer: 2,
-    },
-    {
-        question: 'What does "i" stand for in a loop?',
-        choice1: 'information',
-        choice2: 'index',
-        choice3: 'initiative',
-        choice4: 'initialise',
-        answer: 2,
-    },
-    {
-        question: 'What tag is used to define a standard cell inside a table?',
-        choice1: '<button>',
-        choice2: '<footer>',
-        choice3: '<td>',
-        choice4: '<h1> to <h6>',
-        answer: 3,
-    }
-];
 
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = questions.length;
---
+++ assets/js/modules/questions.js
@@ -0,0 +1,35 @@
+export const questions = [
+    {
+        question: 'What brackets are used for an array?',
+        choice1: '[]',
+        choice2: '()',
+        choice3: '{}',
+        choice4: '<>',
+        answer: 1,
+    },
+    {
+        question: 'What does "var" stand for?',
+        choice1: 'various',
+        choice2: 'variable',
+        choice3: 'varied',
+        choice4: 'varnish',
+        answer: 2,
+    },
+    {
+        question: 'What does "i" stand for in a loop?',
+        choice1: 'information',
+        choice2: 'index',
+        choice3: 'initiative',
+        choice4: 'initialise',
+        answer: 2,
+    },
+    {
+        question: 'What tag is used to define a standard cell inside a table?',
+        choice1: '<button>',
+        choice2: '<footer>',
+        choice3: '<td>',
+        choice4: '<h1> to <h6>',
+        answer: 3,
+    },
+    // Add more questions as needed
+];
